1d065f1d6af389ba5828fc1b2a61061a4f00eccf
nandinidas07 <nandinidas107@gmail.com>
2025-08-01 22:14:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const form = document.getElementById('preferencesForm');
+const fontSizeSelect = document.getElementById('fontsize');
+const fontColorSelect = document.getElementById('fontcolor');
+
+window.addEventListener('DOMContentLoaded', () => {
+  const savedFontSize = localStorage.getItem('fontSize');
+  const savedFontColor = localStorage.getItem('fontColor');
+
+  if (savedFontSize) {
+    document.documentElement.style.setProperty('--fontsize', savedFontSize);
+    fontSizeSelect.value = savedFontSize;
+  }
+
+  if (savedFontColor) {
+    document.documentElement.style.setProperty('--fontcolor', savedFontColor);
+    fontColorSelect.value = savedFontColor;
+  }
+});
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const fontSize = fontSizeSelect.value;
+  const fontColor = fontColorSelect.value;
+
+  localStorage.setItem('fontSize', fontSize);
+  localStorage.setItem('fontColor', fontColor);
+
+  document.documentElement.style.setProperty('--fontsize', fontSize);
+  document.documentElement.style.setProperty('--fontcolor', fontColor);
+});
